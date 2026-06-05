1bddd306fe7dd0d80362cbb9b1fb185bbf6a1b3e
Julien Selling <contact@julienselling.com>
2026-06-05 19:17:03 +0200

    op

--- js/data.js
+++ js/data.js
@@ -5,8 +5,7 @@ const productsData = [
       car: "Ferrari 296 GT3",
       circuit: "Spa-Francorchamps",
       price: 14.99,
-      // Vraie Ferrari 296 GT3 (Rouge/Carbone)
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ferrari_296_GT3_IAA_2023_1X7A0300.jpg/800px-Ferrari_296_GT3_IAA_2023_1X7A0300.jpg",
+      image: "images/ferrari-296-gt3.png",
       featured: true,
       hot: true
     },
@@ -16,8 +15,7 @@ const productsData = [
       car: "Porsche 963 LMDh",
       circuit: "Le Mans",
       price: 12.99,
-      // Vraie Porsche 963
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Porsche_963_IAA_2023_1X7A0265.jpg/800px-Porsche_963_IAA_2023_1X7A0265.jpg",
+      image: "images/porsche-963-lmdh.png",
       featured: true,
       hot: false
     },
@@ -27,8 +25,7 @@ const productsData = [
       car: "BMW M4 GT3",
       circuit: "Nürburgring",
       price: 14.99,
-      // Vraie BMW M4 GT3 (Livrée M Motorsport)
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/BMW_M4_GT3_IAA_2023_1X7A0222.jpg/800px-BMW_M4_GT3_IAA_2023_1X7A0222.jpg",
+      image: "images/bmw-m4-gt3.png",
       featured: true,
       hot: true
     },
@@ -38,8 +35,7 @@ const productsData = [
       car: "Ferrari 499P LMH",
       circuit: "Monza",
       price: 12.99,
-      // Vraie Ferrari 499P (Gagnante Le Mans)
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/2023_Ferrari_499P_No.50.jpg/800px-2023_Ferrari_499P_No.50.jpg",
+      image: "images/ferrari-499p.png",
       featured: false,
       hot: false
     },
@@ -49,8 +45,7 @@ const productsData = [
       car: "Porsche 992 GT3 R",
       circuit: "Suzuka",
       price: 14.99,
-      // Vraie Porsche 911 GT3 R type 992
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Porsche_911_GT3_R_%28992%29_Genf_2024_1Y7A5223.jpg/800px-Porsche_911_GT3_R_%28992%29_Genf_2024_1Y7A5223.jpg",
+      image: "images/porsche-992-gt3r.png",
       featured: false,
       hot: false
     }
